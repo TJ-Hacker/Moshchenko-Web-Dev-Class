bb9ec5f0ba8f110eedc027ef4683c031509fa82b
Tanvir Jawad <Tjawad6912@bths.edu>
2022-12-06 09:20:44 -0500

    O Mesh, Raycasting

--- P4_3DTicTacToe/3D.js
+++ P4_3DTicTacToe/3D.js
@@ -6,42 +6,102 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+const raycaster = new THREE.Raycaster();
+raycaster.layers.set(5);
+const pointer = new THREE.Vector2();
+
+pointer.x = 1;
+pointer.y = 1;
+
 // Geometries
-const plane = new THREE.BoxGeometry(gridSize, 0.05, gridSize);
-// const xPiece = new THREE.BoxGeometry(1, 1, 1);
-const xPiece = new THREE.SphereGeometry(0.5, 32, 16);
-const oPiece = new THREE.SphereGeometry(0.5, 32, 16);
+
+const groundLength = gridSize * 25;
+
+const colliderPlane = new THREE.BoxGeometry(groundLength, 0.05, groundLength);
+
+const vert = new THREE.BoxGeometry(groundLength, 2, 2);
+vert.translate(0, 0, 12);
+const vert2 = new THREE.BoxGeometry(groundLength, 2, 2);
+vert2.translate(0, 0, -12);
+const hori = new THREE.BoxGeometry(2, 2, groundLength);
+hori.translate(12, 0, 0);
+const hori2 = new THREE.BoxGeometry(2, 2, groundLength);
+hori2.translate(-12, 0, 0);
+
+const X_Geo = new THREE.SphereGeometry(0.5, 32, 16);
+const O_Geo = new THREE.TorusGeometry(8, 2, 12, 40);
+O_Geo.rotateX(Math.PI / 2);
 
 // Materials
-const xPiece_mat = new THREE.MeshStandardMaterial({ color: 0x4287f5 });
-const oPiece_mat = new THREE.MeshStandardMaterial({ color: 0xb03131 });
-const ghost_mat = new THREE.MeshStandardMaterial({ color: 0xffffff });
+const Ground_Mat = new THREE.MeshStandardMaterial({ color: 0xffffff });
+const X_Mat = new THREE.MeshStandardMaterial({ color: 0x33ebeb });
+const O_Mat = new THREE.MeshStandardMaterial({ color: 0xeb3434 });
+const invis_Mat = new THREE.MeshStandardMaterial({ color: 0xffffff });
+
+invis_Mat.transparent = true;
+invis_Mat.opacity = 0.1;
+
+O_Mat.flatShading = true;
+// O_Mat.transparent = true;
+// O_Mat.opacity = 0.4;
 
 // Lighting
-const mainPointL = new THREE.PointLight(0xffffff, 1, 100);
+const mainPointL = new THREE.PointLight(0xffffff, 1, 100); // Main Lighting
 mainPointL.position.set(0, 30, 10);
 
+const light = new THREE.AmbientLight(0x404040, 0.5); // Ambient Lighting
+
 // Meshes
-const ground = new THREE.Mesh(plane, ghost_mat);
+
+const v1 = new THREE.Mesh(vert, Ground_Mat);
+const v2 = new THREE.Mesh(vert2, Ground_Mat);
+const h1 = new THREE.Mesh(hori, Ground_Mat);
+const h2 = new THREE.Mesh(hori2, Ground_Mat);
+
+const collider = new THREE.Mesh(colliderPlane, invis_Mat);
+collider.layers.set(5);
 
 xP = [];
 oP = [];
 
-scene.add(ground);
+scene.add(v1);
+scene.add(v2);
+scene.add(h1);
+scene.add(h2);
 scene.add(mainPointL);
+scene.add(light);
+scene.add(collider);
 
-camera.position.z = 3.4;
+camera.position.z = 75;
 
 function animate() {
     requestAnimationFrame(animate);
+
+    raycaster.setFromCamera( pointer, camera );
+    const intersects = raycaster.intersectObjects( scene.children );
+
+    if (intersects.length > 0) {
+        console.log(intersects[0].point);
+    }
+
     renderer.render(scene, camera);
 }
 animate();
+
+
+function onPointerMove( event ) {
+	pointer.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+	pointer.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+}
 
 addEventListener("mousemove", (event) => {
     if (event.buttons == 1) {
         cameraMovement(event.movementX, event.movementY);
     }
+});
+
+addEventListener("pointermove", (event) => {
+    onPointerMove(event);
 });
 
 function cameraMovement(movementX, movementY) {
@@ -51,7 +111,7 @@ function cameraMovement(movementX, movementY) {
 
 function addPiece(player, tile, second=false) {
     if (player == "X") {
-        let piece = new THREE.Mesh(xPiece, xPiece_mat);
+        let piece = new THREE.Mesh(X_Geo, X_Mat);
         xP[xP.length] = piece;
         scene.add(xP[xP.length-1]);
         xP[xP.length-1].position.x = tile[1]-1;
@@ -60,7 +120,7 @@ function addPiece(player, tile, second=false) {
             xP[xP.length-1].position.y = 1;
         }
     } else {
-        let piece = new THREE.Mesh(oPiece, oPiece_mat);
+        let piece = new THREE.Mesh(O_Geo, O_Mat);
         oP[oP.length] = piece;
         scene.add(oP[oP.length-1]);
         oP[oP.length-1].position.x = tile[1]-1;
@@ -70,3 +130,5 @@ function addPiece(player, tile, second=false) {
         }
     }
 }
+
+move("O", [1, 1]);
